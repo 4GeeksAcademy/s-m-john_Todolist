7d74b34db4180523dde97744c987f6ae396ef80b
s-m-john <107650644+s-m-john@users.noreply.github.com>
2023-07-01 01:08:43 +0000

    updates

--- src/js/component/TodoContainer.jsx
+++ src/js/component/TodoContainer.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import Todo from './Todo.jsx'
 
-export default function todocontainer() {
+import { Context } from '../Context.jsx'
 
-    const [useInput, setUserInput] = useState("");
-    const [todoList, setTodoList] = useState([]);
+export default function TodoContainer() {
+
+    const [todoList, setTodoList] = useContext("Context");
+    const [userInput, setUserInput] = useState("");
+    
     // const [todoList, setTodoList] = useState([])
     const onChangeHandler = (event) => setUserInput(event.target.value);
 
     const removeTodo = (index) => {
+        setTodoList((previousTodos) => previousTodos.filter((_,idx) => index !== idx))
 
         // const filteredTodos = [];
         
@@ -32,4 +36,17 @@ export default function todocontainer() {
             setUserInput("");
         }
     }
-}
+
+return (
+    <div>
+        <form onSubmit={(e) => e.preventDefault()}>
+            <input
+                value={userInput}
+                onChange={onChangeHandler}
+                onKeyUp={addTodoHandler}
+            />
+        </form>
+
+        {todoList.map((todo, index) => <Todo key={index} index={index} todo={todo} removeTodo={removeTodo} /}
+    </div>
+)
